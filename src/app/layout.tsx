75fc0edd0f1648130462ce5b994267c061020676
Carlos Masson <carlosmasson96@gmail.com>
2023-11-22 23:01:34 -0500

    schema markup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,6 +26,33 @@ const poppins = Poppins({
   variable: '--font-poppins',
 })
 
+const schema = {
+    "@context": "https://schema.org/",
+    "@type": "Person",
+    "name": "Todd Nepola",
+    "url": "https://toddnepola.com",
+    "image": "https://toddnepola.com/images/square.png",
+    "sameAs": [
+      "https://www.facebook.com/todd.nepola/",
+      "https://www.instagram.com/lifeaccordingtotodd",
+      "https://www.youtube.com/channel/UCBb9T0yL77zmLOQMhg9njNw",
+      "https://www.linkedin.com/in/todd-nepola-52323441/",
+      "https://toddnepola.com"
+    ],
+    "jobTitle": "President",
+    "worksFor": {
+      "@type": "Organization",
+      "name": "Current Capital Real Estate Group"
+    } ,
+    "alumniOf": "University of South Florida" ,
+    "birthDate": "November 19, 1972",
+    "birthPlace": "Staten Island, New York, NY",
+    "children": ["Gabriella Nepola", "Sophia Nepola"],
+    "parents": ["Ina Nepola", "Thomas Nepola"],
+    "sibling": "Justin Nepola",
+
+}
+
 export const metadata: Metadata = {
   metadataBase: new URL('https://toddnepola.com/'),
   title: 'Todd Nepola',
@@ -51,6 +78,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html className="selection:bg-primary/70 selection:text-white scroll-smooth" lang="en">
       <Script src="https://www.googletagmanager.com/gtag/js?id=G-R62FZS8CSQ" />
+      <Script id="structured-schema" type="application/ld+json" dangerouslySetInnerHTML={{ __html: JSON.stringify(schema) }} />
       <Script id="google-analytics">
         {`
           window.dataLayer = window.dataLayer || [];
